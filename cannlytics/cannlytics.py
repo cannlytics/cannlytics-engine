4f2a59cf2a4217086c1344e94f0696ae7f3bc41c
Keegan Skeate <keeganskeate@gmail.com>
2021-10-29 20:31:33 -0600

    Added empty config to Cannlytics instance

--- cannlytics/cannlytics.py
+++ cannlytics/cannlytics.py
@@ -9,17 +9,25 @@ from typing import Dict, List, Optional, Type, Union
 
 from enforce_typing import enforce_types
 
-logger = logging.getLogger('cannlytics')
+# logger = logging.getLogger('cannlytics')
 
 
 class Cannlytics:
     """An instance of this class is the entry point into Cannlytics."""
 
     @enforce_types
-    def __init__(self) -> None:
+    def __init__(self, config: Dict) -> None:
         """Initialize Cannlytics class.
+
         Usage: Make a new Cannlytics instance
+
         `cannlytics = Cannlytics({...})`
+
         This class provides the main top-level functions in Cannlytics.
         """
-        logger.debug('Cannlytics instance initialized.')
+        # logger.debug('Cannlytics instance initialized.')
+
+        if isinstance(config, dict):
+            config_dict = {}
+            config = {**config_dict, **config}
+        self.config = config
